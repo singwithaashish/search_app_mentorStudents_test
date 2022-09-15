c2d13a70089a149248f0f7ddf8b40da67650f25e
singwithaashish <singwithaashish@gmail.com>
2022-09-15 10:26:28 +0530

    Added headline & description in search query

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const Ads = require("./schema/adSchema");
 
-
 const app = express();
 
 // connecting to mongodb atlas, shared cluster
@@ -30,7 +29,6 @@ const db = mongoose.connection;
 app.use(bodyParser.json());
 app.use(cors());
 
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -39,30 +37,38 @@ app.get("/", (req, res) => {
 app.post("/post", async (req, res) => {
   // console.log(req.body);
   const str = req.body.str;
-    // if(!str){
-    //     res.status(400).json({message:"Please enter a keyword"});
-    //     // res.send("Please enter a valid keyword");
-    //     return;
-    // }
+  // if(!str){
+  //     res.status(400).json({message:"Please enter a keyword"});
+  //     // res.send("Please enter a valid keyword");
+  //     return;
+  // }
 
-    // get ads and company data from the database and merge them and send it to the frontend
+  // get ads and company data from the database and merge them 
+  // with respective company info and send it to the frontend
 
-    const ads = await Ads.find({ primaryText: { $regex: str, $options: "i" } });
-    // also merge the company data with the ads data if the company id matches
-    const companyData = await db.collection("company").find().toArray();
-    console.log(companyData);
-    const mergedData = ads.map((ad) => {
-      //  compare each ad with the company data and merge them
-        const company = companyData.filter((company) => { 
-            return company._id.equals(ad.toObject().company_id)
-        });
-        console.log(company);
-        return { ...ad._doc, ...company[0] };
+  const ads = await Ads.find({
+    $or: [
+      { primaryText: { $regex: str, $options: "i" } },
+      { headline: { $regex: str, $options: "i" } },
+      { description: { $regex: str, $options: "i" } },
+    ],
+  });
+  const companyData = await db.collection("company").find().toArray();
+  // console.log(companyData);
+  
+  // also merge the company data with the ads data if the company id matches
+  //lookup would work better here but it's not working so i'm using a for loop
+  const mergedData = ads.map((ad) => {
+    //  compare each ad with the company data and merge them
+    const company = companyData.filter((company) => {
+      return company._id.equals(ad.toObject().company_id);
     });
+    // console.log(company);
+    return { ...ad._doc, ...company[0] };
+  });
 
-    // const company = await Company.find({ name: { $regex: str, $options: "i" } });
-    console.log(mergedData)
-    // const data = [...ads, ...company];
-    res.send(mergedData);
-
+  // if we are searching for company name too, then 
+  // we can use the below code and another for loop to match and merge the data with ads
+  // const company = await Company.find({ name: { $regex: str, $options: "i" } });
+  res.send(mergedData);
 });
